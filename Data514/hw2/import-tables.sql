83af2234f31263b42b3f601f617994c838c8591f
Naomi Liftman <nliftman@smith.edu>
2026-04-14 23:49:46 -0700

    updating hw 2 wil hoepfully finish tomorrow

--- Data514/hw2/import-tables.sql
+++ Data514/hw2/import-tables.sql
@@ -3,6 +3,6 @@ PRAGMA foreign_keys=ON;
 .import --skip 1 /Users/naomiliftman/Desktop/MSDS/Data514/hw2/flights-csv/aircraft_types.csv Aircraft_Types
 .import --skip 1 /Users/naomiliftman/Desktop/MSDS/Data514/hw2/flights-csv/cancellation_codes.csv Cancellation_Codes
 .import --skip 1 /Users/naomiliftman/Desktop/MSDS/Data514/hw2/flights-csv/carriers.csv Carriers
-.import --skip 1 /Users/naomiliftman/Desktop/MSDS/Data514/hw2/flights-csv/flights-2024-09.csv Flights
 .import --skip 1 /Users/naomiliftman/Desktop/MSDS/Data514/hw2/flights-csv/n_numbers.csv N_Numbers
+.import --skip 1 /Users/naomiliftman/Desktop/MSDS/Data514/hw2/flights-csv/flights-2024-09.csv Flights
 
